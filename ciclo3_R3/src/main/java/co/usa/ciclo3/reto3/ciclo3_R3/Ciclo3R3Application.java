fc34e573043a8d36955ae338965ce6e27c8c1569
Jairo Becerra <jairobecerra.mt@correo.usa.edu.co>
2021-10-17 12:16:43 -0500

    Proyecto retos

---
+++ ciclo3_R3/src/main/java/co/usa/ciclo3/reto3/ciclo3_R3/Ciclo3R3Application.java
@@ -0,0 +1,13 @@
+package co.usa.ciclo3.reto3.ciclo3_R3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ciclo3R3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ciclo3R3Application.class, args);
+	}
+
+}
